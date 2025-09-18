1efe0c648bef91edecda984e6c0b242479b462ac
Paresh singh <pareshsingh@Pareshs-Laptop.local>
2025-09-18 19:33:35 +0530

    fix: task issue

--- src/App.js
+++ src/App.js
@@ -1,40 +1,9 @@
-// import React from "react";
-// import Navbar from "./components/Navbar";
-//  import HeroSection from "./components/HeroSection";
-//  import Features from "./components/Features";
-// import Ratings from "./components/Ratings";
-// import Achievements from "./components/Achievements";
-// import Footer from "./components/Footer";
-// import CoachProfile from "./components/CoachProfile";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Home from "./components/Home";
-// import AboutUs from "./components/AboutUs";
-
-// function App() {
-//   return (
-//     <div>
-//       <Navbar />
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-//         <Route path="/about" element={<AboutUs />} />
-//       </Routes>
-//       <HeroSection />
-//       <Features />
-//       <Ratings />
-//        <CoachProfile />
-//       <Achievements />
-//       <Footer />
-//     </div>
-//   );
-// }
-
-// export default App;
-
+// App.js
 import React from "react";
-import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import AboutUs from "./components/AboutUs";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
@@ -49,4 +18,3 @@ function App() {
 }
 
 export default App;
-
